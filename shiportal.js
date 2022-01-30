4da845174b1cc3b09a16e88ac52f83385737beaf
trunith <64655194+trunith@users.noreply.github.com>
2022-01-30 11:37:26 -0800

    a
    a

---
+++ shiportal.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="shiportal.css">
+<title>shirena///portal</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="shiportal.js"></script>
+<link rel="icon" type="image/png" href="https://cdn.discordapp.com/attachments/775214973089611796/809228871345504256/THECAT-cropv2.png"/>
+</head>
+<body>
+<!-- Start your code here -->
+<img class="ttl" src="https://cdn.discordapp.com/attachments/775214973089611796/809230811546124300/shirena.png"/>
+    <br/><br/><br/><br/><br/>
+    <p class="pintx">**this site is meant to be viewed on a computer**</p>
+    <br/><br/><br/><br/><br/>
+  <p class="heatx">/// shirena™ ///</p><image class="mimg" src="https://cdn.discordapp.com/attachments/775214973089611796/809235018865836043/THECATFULL_POGh.png"/>
+  <a class = "greetx" alt="Invite Shirena to your server" href="https://bit.ly/SHIRENA">invite</a>
+  <br/>
+  <p class="nortx">--medias--</p>
+  <p class="pintx" alt="Other">trunith</p>
+  <button id="p1" class="bremoval" onclick="copyToClipboard('#p1')"><p class="pintx" alt="Discord Tag">cef#7011</p></button>
+<br/>
+  <br/>
+  <p class="nortx">--main--</p>
+  <a class="blutx" alt="Commands for Shirena" href="https://www.trunith.com/shicommands" target="_blank">commands</a>
+  <br/>
+  <a class="blutx" alt="What is Shirena" href="https://www.trunith.com/shiabout" target="_blank">about</a>
+  <br/>
+  <a class="blutx" alt="Who created Shirena" href="https://www.trunith.com/shicreator" target="_blank">creator</a>
+  <br/>
+  <a class="blutx" alt="Other" href="https://www.trunith.com/shiother" target="_blank">others</a>
+  <br/>
+  <p class="nortx">--other--</p>
+  <a class="blutx" alt="People who helped with Shirena" href="https://www.trunith.com/shihelpers"target="_blank">helpers /// special thanks</a>
+<br/>
+  <a class="blutx" alt="placeholdertext" href="https://www.trunith.com/404"target="_blank">[---+ placeholdertext (incase if i add anything else) +---]</a>
+  <br/>
+  <br/>
+  <br/>
+<!-- End your code here -->
+</body>
+</html>
